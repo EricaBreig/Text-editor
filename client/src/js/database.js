24d70567f3ceb00b99f16cc3dcd5355c1b592338
Erica Breig <ericabreig@gmail.com>
2022-07-19 01:11:25 -0400

    edited database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,7 +14,7 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.error("PUT to the database :D");
+  console.log("PUT to the database :D");
   const jateDb = await openDB("jate", 1);
   const tx = jateDb.transaction("jate", "readwrite");
   const store = tx.objectStore("jate");
@@ -25,7 +25,7 @@ export const putDb = async (content) => {
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.error("GET from the database :D ");
+  console.log("GET from the database :D ");
   const jateDb = await openDB("jate", 1);
   const tx = jateDb.transaction("jate", "readonly");
   const store = tx.objectStore("jate");
